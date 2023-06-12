982f34c303424985acfd5a39286fece2e6eb57cc
Tyler E <ty-ty555@live.com>
2023-06-12 12:10:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ const { MongoClient } = require('mongodb');
 
 // Connection URL
 const url = 'mongodb://Eckhatyl000:TeeGee%231@tylerdb.cknlngax0gto.us-west-2.docdb.amazonaws.com:27017/?retryWrites=false';
-const ca = fs.readFileSync('/home/ec2-user/files/us-west-2-bundle.pem');
+const ca = fs.readFileSync('/home/ec2-user/us-west-2-bundle.pem');
 
 const options = {
     useNewUrlParser: true,
