be91bcdaefd7e1837dbc6c96f4771d9619d1e7f3
Tyler E <ty-ty555@live.com>
2023-06-12 11:16:50 -0600

    Update server.js

--- server.js
+++ server.js
@@ -86,15 +86,15 @@ app.listen(port, () => {
 const { MongoClient } = require('mongodb');
 
 // Connection URL
-const url = 'mongodb://Eckhatyl000:TeeGee%231@tylerdb.cknlngax0gto.us-west-2.docdb.amazonaws.com:27017/?ssl=true&ssl_ca_certs=us-west-2-bundle.pem&retryWrites=false';
+const url = 'mongodb://Eckhatyl000:TeeGee%231@tylerdb.cknlngax0gto.us-west-2.docdb.amazonaws.com:27017/?retryWrites=false';
 const ca = fs.readFileSync('/home/ec2-user/files/us-west-2-bundle.pem');
 
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    ssl: true,
-    sslValidate: true,
-    sslCA: ca,
+    tls: true,
+    tlsInsecure: false,
+    tlsCAFile: ca,
 };
 
 
